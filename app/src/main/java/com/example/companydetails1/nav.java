b780a2871956ca4c6d2f2f64895f40cecd7ddf12
Your Name <you@example.com>
2020-10-31 17:37:03 +0530

    initial commit

---
+++ app/src/main/java/com/example/companydetails1/nav.java
@@ -0,0 +1,31 @@
+package com.example.companydetails1;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class nav extends Fragment {
+
+
+    public nav() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nav, container, false);
+        return view;
+    }
+
+}
